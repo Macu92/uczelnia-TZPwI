a14b6cae7be6e0f6b480e4d3a6e47ee3955e176b
Maciej <Maciej>
2016-12-12 19:34:41 +0100

    podzieone ma tedoki

--- Krzywe Eliptyczne/src/krzywe/eliptyczne/ECSystem.java
+++ Krzywe Eliptyczne/src/krzywe/eliptyczne/ECSystem.java
@@ -5,10 +5,97 @@
  */
 package krzywe.eliptyczne;
 
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Maciek
  */
 public class ECSystem {
-    
+
+    public ECPoint generateOpenKey(int privateKey, List<ECPoint> group) {
+        return group.get(privateKey - 1);
+    }
+
+    public ECPoint generateSecretKey(int privateKey, ECPoint openKey, ECGroup group) {
+        List<ECPoint> grup1 = group.generateG(openKey);
+        ECPoint secretKey = null;
+        if (grup1.size() < privateKey) {
+            secretKey = grup1.get((privateKey % (grup1.size() + 1)) - 1);
+        } else {
+            secretKey = grup1.get(privateKey - 1);
+        }
+        return secretKey;
+    }
+
+    public ECPoint sign(String message, ECGroup group, List<ECPoint> generatedGPoints, int privateKey) {
+        Integer s = 0;
+        int r = 0;
+        MessageDigest md;
+        byte[] bytesOfMessage;
+        Integer c = group.getC();
+        do {
+            try {
+                Double randKD = new Double("0");
+                Integer randK = 0;
+                do {
+                    randKD = (Math.random() * 100) % c;
+                    randK = 5;//randKD.intValue()+1;
+                    ECPoint kpoint = generatedGPoints.get(randK - 1);
+                    r = kpoint.x % c;
+                } while (r == 0);
+                System.out.println("bi" + randK + " " + c);
+                Integer kmod = new BigInteger(randK.toString()).modInverse(new BigInteger(c.toString())).intValue();
+                bytesOfMessage = message.getBytes("UTF-8");
+                md = MessageDigest.getInstance("SHA-1");
+                Integer hashMessage = new BigInteger(md.digest(bytesOfMessage)).intValue() % c;
+                Integer temp = hashMessage + privateKey * r;
+                s = (kmod * temp) % c;
+            } catch (NoSuchAlgorithmException ex) {
+                Logger.getLogger(ECSystem.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (UnsupportedEncodingException ex) {
+                Logger.getLogger(ECSystem.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } while (s == 0);
+        return new ECPoint(r, s);
+    }
+
+    public ECPoint verifySIgn(String message, ECPoint signature, ECGroup group, ECPoint openPaKey, List<ECPoint> generatedGPoints) {
+         ECPoint fi = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+            Integer c = group.getC();
+            byte[] bytesOfMessage2 = message.getBytes("UTF-8");
+            Integer hashMessage2 = new BigInteger(md.digest(bytesOfMessage2)).intValue() % c;
+            Integer r2 = signature.x;
+            Integer s2 = signature.y;
+            if (r2 < 1 || r2 > c - 1) {
+                System.out.println("warunek r2 niespelnoiny");
+            }
+            if (s2 < 1 || s2 > c - 1) {
+                System.out.println("wrunek s2 niespelniony");
+            }
+            Integer w = new BigInteger(s2.toString()).modInverse(new BigInteger(c.toString())).intValue();
+            Integer u1 = (hashMessage2 * w) % c;
+            Integer u2 = (r2 * w) % c;
+            List<ECPoint> upa = group.generateG(openPaKey);
+            ECPoint d;
+            if (upa.size() < u2) {
+                d = upa.get((upa.size() % u2) - 1);
+            } else {
+                d = upa.get(u2 - 1);
+            }
+            System.out.println(d.toString() + " " + generatedGPoints.get(u1).toString());
+            fi = group.add2(generatedGPoints.get(u1 - 1), d);
+        } catch (Exception ex) {
+            Logger.getLogger(ECSystem.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return fi;
+    }
 }
--- Krzywe Eliptyczne/src/krzywe/eliptyczne/KrzyweEliptyczne.java
+++ Krzywe Eliptyczne/src/krzywe/eliptyczne/KrzyweEliptyczne.java
@@ -41,80 +41,25 @@ public class KrzyweEliptyczne {
             }
             System.out.println("Group Point P" + i + "(O)");
             System.out.println("Rzad:" + group.getC());
+            
+            ECSystem ecsys = new ECSystem();
+            
             int na = getInt("Podaj liczbę(secret key A) mniejsza niż " + i);
-            ECPoint pa = grup.get(na - 1);
+            ECPoint pa = ecsys.generateOpenKey(na, grup);
             int nb = getInt("Podaj liczbę(secret key B) mniejsza niż " + i);
-            ECPoint pb = grup.get(nb - 1);
-            List<ECPoint> grup1 = group.generateG(pa);
-            List<ECPoint> grup2 = group.generateG(pb); //35/25/33 19,38  //60,11,17
-            ECPoint pp;
-             if (grup1.size() < nb) {
-                pp = grup1.get((nb%(grup1.size()+1)) - 1);
-            } else {
-                pp= grup1.get(nb - 1);
-            }
-            System.out.println("Klucz wspolny na*Pb=na*(nb*G):" + na + "*P" + pb.toString() + "=" + pp.toString());
-            ECPoint pbb;
-             if (grup2.size() < na) {
-                pbb = grup2.get((na%(grup2.size()+1) ) - 1);
-            } else {
-                pbb = grup2.get(na - 1);
-            }
-            System.out.println("Klucz wspolny nb*Pa=nb*(na*G):" + nb + "*P" + pa.toString() + "=" + pbb.toString());
-//            System.out.println("ZOBACZMY CO WYSZLo");
+            ECPoint pb =  ecsys.generateOpenKey(nb, grup);
+            
+            ECPoint userASecretKey = ecsys.generateSecretKey(na,pb,group);
+            System.out.println("Klucz wspolny na*Pb=na*(nb*G):" + na + "*P" + pb.toString() + "=" + userASecretKey.toString());
+            ECPoint userBSecretKey = ecsys.generateSecretKey(nb,pa,group);
+            System.out.println("Klucz wspolny nb*Pa=nb*(na*G):" + nb + "*P" + pa.toString() + "=" + userBSecretKey.toString());
 
-            //coding
-            Integer s = 0;
-            int r = 0;
-            MessageDigest md;
-            byte[] bytesOfMessage;
-            Integer c = group.getC();
-            do {
-                Double randKD = new Double("0");
-                Integer randK = 0;
-                do {
-                    randKD = (Math.random() * 100) % c;
-                    randK = 5;//randKD.intValue()+1;
-                    ECPoint kpoint = grup.get(randK-1);
-                    r = kpoint.x % c;
-                } while (r == 0);
-                System.out.println("bi"+randK+" "+c);
-                Integer kmod = new BigInteger(randK.toString()).modInverse(new BigInteger(c.toString())).intValue();
-                bytesOfMessage = "MOJ SUPER STRING".getBytes("UTF-8");
-                md = MessageDigest.getInstance("SHA-1");
-                Integer hashMessage = new BigInteger(md.digest(bytesOfMessage)).intValue() % c;
-                Integer temp = hashMessage + na * r;
-                s = (kmod * temp) % c;
-            } while (s == 0);
-            ECPoint pair = new ECPoint(r, s);
-
-            // decoding
-            byte[] bytesOfMessage2 = "MOJ SUPER STRING".getBytes("UTF-8");
-            MessageDigest md2 = MessageDigest.getInstance("SHA-1");
-            Integer hashMessage2 = new BigInteger(md.digest(bytesOfMessage)).intValue() % c;
-            Integer r2 = pair.x;
-            Integer s2 = pair.y;
-            if (r2 < 1 || r2 > c - 1) {
-                System.out.println("warunek r2 niespelnoiny");
-            }
-            if (s2 < 1 || s2 > c - 1) {
-                System.out.println("wrunek s2 niespelniony");
-            }
-            Integer w = new BigInteger(s2.toString()).modInverse(new BigInteger(c.toString())).intValue();
-            Integer u1 = (hashMessage2 * w) % c;
-            Integer u2 = (r2 * w) % c;
-            List<ECPoint> upa = group.generateG(pa);
-            ECPoint d;
-            if (upa.size() < u2) {
-                d = upa.get((upa.size() % u2) - 1);
-            } else {
-                d = upa.get(u2 - 1);
-            }
-            System.out.println(d.toString()+" "+grup.get(u1).toString());
-            ECPoint fi = group.add2(grup.get(u1 - 1), d);
-
-            System.out.println("R:" + r + " R*:" + fi.x % c);
-
+//            //sign
+            ECPoint signature =  ecsys.sign("MOJ SUPER STRING", group, grup, na);
+            // verify
+            ECPoint verified = ecsys.verifySIgn("MOJ SUPER STRING", signature, group, pa, grup);
+            System.out.println("R:" + signature.x + " R*:" + verified.x % group.getC());
+//
         } catch (Exception ex) {
             Logger.getLogger(KrzyweEliptyczne.class.getName()).log(Level.SEVERE, null, ex);
         }
